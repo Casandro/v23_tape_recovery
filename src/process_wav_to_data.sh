c80be5c366caa66c831a57e2b62106493e299cb8
Christian Berger <christian@clarke-3.de>
2022-10-02 23:04:14 +0200

    added feature to generate preview images of cept files if btx_decoder is installed in ../../btx_decoder

--- src/process_wav_to_data.sh
+++ src/process_wav_to_data.sh
@@ -18,11 +18,25 @@ OUT="`basename "$1" .wav`.zip"
 
 
 tmp=`mktemp /tmp/XXXXXXX.raw`
-sox "$IN" -c 1 -r 6800 -e floating-point -b 64 -t raw - sinc 400 -n 8192 | ./demod_double | sox -c 1 -r 3400 -e floating-point -b 64 -t raw - -r 19200 -t raw $tmp sinc -800  -n 8192 
-cp $tmp /tmp/temp.raw
+sox "$IN" -c 1 -r 6800 -e floating-point -b 64 -t raw - sinc 300 -n 8192 | ./demod_double | sox -c 1 -r 3400 -e floating-point -b 64 -t raw - -r 19200 -t raw $tmp sinc -800  -n 8192
 ./uart $tmp  > $tmp.timedoctets
 wc -l $tmp.timedoctets
-rm /tmp/split/*
-./split_to_to_files /tmp/split/ < $tmp.timedoctets
+OUT_DIR=$tmp.outdir/
+mkdir -p $OUT_DIR
+./split_to_to_files $OUT_DIR < $tmp.timedoctets
+if [ -e ../../btx_decoder/to_image/cept_to_image ]
+then
+	for f in $OUT_DIR/*.cpt
+	do
+		../../btx_decoder/to_image/cept_to_image  $f $f.pnm
+		convert $f.pnm $f.png
+		rm $f.pnm
+	done
+fi
+
+mv $tmp.timedoctets $tmp.outdir/timed_octets.txt
+
+zip -j -n .png "$IN".zip $tmp.outdir/*
+echo $tmp.outdir/
+rm -r $tmp.outdir/
 rm $tmp
-rm $tmp.timedoctets
