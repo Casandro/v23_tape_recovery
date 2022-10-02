584a54d7f5ca741ef3891590631e939281be770c
Christian Berger <christian@clarke-3.de>
2022-10-02 23:03:27 +0200

    Changed name schema for output files to minutes:seconds

--- src/split_to_to_files.c
+++ src/split_to_to_files.c
@@ -14,15 +14,15 @@ int main(int argc, char *argv[])
 	double t=0;
 	int o=0;
 	double start=0;
-	double end=0;
 	int cnt=0;
 	while (scanf("%lf%d", &t, &o)==2) {
 		double td=t-lt;
 		if (td>0.05) {
-			end=lt;
 			if (cnt>0) {
 				char fn[256];
-				snprintf(fn, sizeof(fn)-1, "%s%04.2lf-%04.2lf.cpt", prefix, start,end);
+				int s=start;
+				int minute=(s/60);
+				snprintf(fn, sizeof(fn)-1, "%s%02d:%05.2lf.cpt", prefix, minute, start-minute*60);
 				FILE *f=fopen(fn, "w");
 				fwrite(buffer, 1, cnt, f);
 				fclose(f);
