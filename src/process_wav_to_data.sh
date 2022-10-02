dba558e28a4aa92fbe69d3ea61045b1724adc9cb
Christian Berger <christian@clarke-3.de>
2022-10-02 22:11:34 +0200

    Added minor filtering to improve SNR

--- src/process_wav_to_data.sh
+++ src/process_wav_to_data.sh
@@ -13,13 +13,16 @@ done
 
 
 IN="$1"
+OUT="`basename "$1" .wav`.zip"
+
 
 
 tmp=`mktemp /tmp/XXXXXXX.raw`
-sox "$IN" -c 1 -r 6800 -e floating-point -b 64 -t raw - | ./demod_double | sox -c 1 -r 3400 -e floating-point -b 64 -t raw - -r 19200 -t raw  $tmp
+sox "$IN" -c 1 -r 6800 -e floating-point -b 64 -t raw - sinc 400 -n 8192 | ./demod_double | sox -c 1 -r 3400 -e floating-point -b 64 -t raw - -r 19200 -t raw $tmp sinc -800  -n 8192 
 cp $tmp /tmp/temp.raw
 ./uart $tmp  > $tmp.timedoctets
 wc -l $tmp.timedoctets
+rm /tmp/split/*
 ./split_to_to_files /tmp/split/ < $tmp.timedoctets
 rm $tmp
 rm $tmp.timedoctets
